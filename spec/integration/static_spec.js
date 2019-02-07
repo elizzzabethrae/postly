555781067c5dfdab809e780465ff6397cd073b9d
elizzzabethrae <elizabethwarners@gmail.com>
2019-02-07 12:49:30 -0500

    added route test

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,15 +5,25 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-
     it("should return status code 200", (done) => {
-
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
         done();
       });
     });
-
   });
+
+  describe("route: static", () => {
+
+   describe("GET /marco", () => {
+     it("should return status code 200", (done) => {
+       request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200) &&
+        expect(res.body).toContain("polo");
+         done();
+       });
+     });
+    });
+  });
+
 });
