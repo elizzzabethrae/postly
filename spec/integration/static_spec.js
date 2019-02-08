9ed1dce9b9ebb931acfd5e98bfd2bbd862a76fed
elizzzabethrae <elizabethwarners@gmail.com>
2019-02-07 21:07:32 -0500

    model view controllers done

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,25 +5,15 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Postly' in the body", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Postly");
         done();
       });
     });
   });
 
-  describe("route: static", () => {
 
-   describe("GET /marco", () => {
-     it("should return status code 200", (done) => {
-       request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200) &&
-        expect(res.body).toContain("polo");
-         done();
-       });
-     });
-    });
-  });
 
 });
--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+
 const express = require("express");
 const app = express();
 
+
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 routeConfig.init(app);
+appConfig.init(app, express);
+
 
 module.exports = app;
